aa8acab18bf5b47db99bbe13c892ff7c9ee82edc
Rúben Almeida <almeidaruben35@gmail.com>
2023-02-17 17:21:17 +0000

    Timer_test_time_base done

--- lab2/timer.c
+++ lab2/timer.c
@@ -7,10 +7,60 @@
 
 int(timer_set_frequency)(uint8_t timer, uint32_t freq)
 {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+  if (freq > TIMER_FREQ)
+  {
+    printf("Invalid frequency!\n");
+    return -1;
+  }
+
+  uint8_t st = 0x00;
+
+  // Read current timer configuration
+  if (timer_get_conf(timer, &st))
+  {
+    printf("Error reading timer configuration!\n");
+    return -1;
+  }
+
+
+  // Select LSB of Status Register
+  uint8_t first_four_st_bits = st & (BIT(0) | BIT(1) | BIT(2) | BIT(3));
+
+  // Set Operating Mode
+  uint8_t initialization_mode = TIMER_LSB_MSB;
+
+  // Set Timer Register
+  uint8_t timer_register = (TIMER_0 + timer) << 6;
+  
+  // Construct Control Word
+  uint8_t control_word = timer_register | first_four_st_bits | initialization_mode;
+
+  // Write Control Word to Control Register
+  if (sys_outb(TIMER_CTRL, (uint8_t)control_word))
+  {
+    printf("Error writing to control register!\n");
+    return -1;
+  }
+
+  // Calculate Timer Initial Value
+  uint16_t initial_value = TIMER_FREQ / freq;
+
+  // Write Timer Initial LSB Value To Timer Register
+  if (sys_outb(TIMER_0 + timer, (uint8_t)initial_value))
+  {
+    printf("Error writing LSB to timer register!\n");
+    return -1;
+  }
+
+  // Write Timer Initial MSB Value To Timer Register
+  if (sys_outb(TIMER_0 + timer, (uint8_t)(initial_value >> 8)))
+  {
+    printf("Error writing MSB to timer register!\n");
+    return -1;
+  }
+
+  return 0;
 }
 
 int(timer_subscribe_int)(uint8_t *bit_no)
